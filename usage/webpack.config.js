97707066376725fc453092f31ad872120be6b56d
chenyinkai <287696322@qq.com>
2018-04-03 22:07:46 +0800

    usage

---
+++ usage/webpack.config.js
@@ -0,0 +1,30 @@
+const webpack = require('webpack')
+const path = require('path')
+const UglifyPlugin = require('uglifyjs-webpack-plugin')
+const cleanPlugin = require('clean-webpack-plugin')
+module.exports = {
+  entry: './index.js',
+  output: {
+    path: path.resolve(__dirname, 'dist'),
+    filename: 'bundle.js'
+  },
+  module: {
+    rules: [
+      {
+        test: /\.jsx?/, // 支持 js 和 jsx
+        loader: 'babel-loader'
+      },
+      {
+        test: /\.css/,
+        use: ['style-loader', 'css-loader']
+      }
+    ]
+  },
+  // 代码模块路径解析的配置
+  resolve: {
+    modules: ['node_modules', path.resolve(__dirname, 'src')],
+
+    extensions: ['.wasm', '.mjs', '.js', '.json', '.jsx']
+  },
+  plugins: [new UglifyPlugin(), new cleanPlugin()]
+}
